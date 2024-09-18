e6727dd7046e880752263d34fe2f0436c2517ec1
MechanicalDeathtrap <korikova-04@mail.ru>
2024-09-18 17:50:06 +0400

    Added Main Banner component, Rubik font connected

---
+++ src/components/main banner/main-banner.tsx
@@ -0,0 +1,17 @@
+import styles from "./main-bunner.module.sass"
+
+export const MainBanner = () => {
+  return(
+    <section className={styles["main-banner"]}>
+      <div className={styles["main-banner__main-image-container_darkening"]}>
+        <img className={styles["main-banner__main-image"]} src="src/assets/img/background.png" alt="banner-photo" />
+      </div>
+      <h1 className={styles["main-banner__main-heading"]}>
+        <span className={styles["main-banner__main-heading_highlighted"]}>объединяем </span>
+         талантливых разработчиков, помогаем им
+        <span className={styles["main-banner__main-heading_highlighted"]}> развиваться</span> и
+        <span className={styles["main-banner__main-heading_highlighted"]}> творить</span>
+      </h1>
+    </section>
+  )
+}
--- src/pages/homepage/homepage.tsx
+++ src/pages/homepage/homepage.tsx
@@ -1,6 +1,9 @@
+import { MainBanner } from "../../components/main banner/main-banner.tsx";
 
 export const HomePage = () =>{
   return(
-    <></>
+    <>
+      <MainBanner/>
+    </>
   )
 }
