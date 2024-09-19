3a11636f6766bc75d9cbf53b9472f912813ec901
MechanicalDeathtrap <korikova-04@mail.ru>
2024-09-19 20:53:43 +0400

    Added AboutUs component

--- src/pages/homepage/homepage.tsx
+++ src/pages/homepage/homepage.tsx
@@ -1,9 +1,11 @@
 import { MainBanner } from "../../components/main banner/main-banner.tsx";
+import { AboutUs } from "../../components/about us/about-us.tsx";
 
 export const HomePage = () =>{
   return(
     <>
       <MainBanner/>
+      <AboutUs/>
     </>
   )
 }
