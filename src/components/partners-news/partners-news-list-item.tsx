18f4c8cd10b5074f87c9767281333c809d1dc24f
MechanicalDeathtrap <korikova-04@mail.ru>
2024-11-08 19:04:31 +0300

    Added link to "Подробнее" button

--- src/components/partners-news/partners-news-list-item.tsx
+++ src/components/partners-news/partners-news-list-item.tsx
@@ -1,4 +1,5 @@
 import styles from "./partners-news-list-item.module.sass"
+import { Link } from "react-router-dom";
 
 export const PartnersNewsListItem = () =>{
   return(
@@ -10,10 +11,14 @@ export const PartnersNewsListItem = () =>{
           <p>Координатор абитуриентов КФУ поговорила о перспективах развития IT: Развитие неоднозначно, но...</p>
         </div>
         <button className={styles["partner-news-item__more-button"]}>
-          <span>Подробнее</span>
-          <svg xmlns="http://www.w3.org/2000/svg" width="12" height="5" viewBox="0 0 12 5" fill="none">
-            <path d="M12 2.5L8.25 0.334936V4.66506L12 2.5ZM0 2.875H8.625V2.125H0V2.875Z" fill="black" />
-          </svg>
+          {/*TODO линк к новости*/}
+          <Link to="/">
+            <span>Подробнее</span>
+            <svg xmlns="http://www.w3.org/2000/svg" width="12" height="5" viewBox="0 0 12 5" fill="none">
+              <path d="M12 2.5L8.25 0.334936V4.66506L12 2.5ZM0 2.875H8.625V2.125H0V2.875Z" fill="black" />
+            </svg>
+          </Link>
+
         </button>
       </div>
     </div>
