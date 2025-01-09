1387cc1ee556b6836302cc8931eb6196e215a080
MechanicalDeathtrap <korikova-04@mail.ru>
2025-01-09 21:46:56 +0300

    style(select-filter, multi-select-filter): changed border style feat(): added searchbar, popularity filter, projects tile, load projects button, added Inter font fix(): changed button html structure component

---
+++ src/components/projects/project-card/project-card.tsx
@@ -0,0 +1,42 @@
+import styles from "../project-card/project-card.module.sass";
+import { Link } from "react-router-dom";
+
+
+export const ProjectCard = () =>{
+  return(
+    <Link to={'/'} >
+      <div className={styles["project-card"]}>{/*TODO достать всё из бэка */}
+        <div className={`${styles["project-card__header"]} ${styles["margin-sides"]}`}>
+          <h6 className={styles["project-card__company-logo"]}>
+            <picture>
+              <img src="/img/Tatneft_Logo.svg" alt="company-logo" />
+            </picture>
+            ОАО Татнефть
+          </h6>
+
+          <ul className={styles["project-card__tech-stack"]}>
+            <li>Node.js</li>
+            <li>React</li>
+            <li>Redux</li>
+          </ul>
+        </div>
+        <div className={`${styles["project-card__main-info"]} ${styles["margin-sides"]}`}>
+          <h2>Составление документации для рабочей API, а также разработка сайта для блаблаблаблаблаблаблабалбалабла
+            типа много текста проверим css </h2>
+          <ul className={styles["project-card__main-info-list"]}>
+            <li> Сроки работы:
+              <span>20 минут</span>
+            </li>
+            <li>Бюджет:
+              <span>1 337 000 р</span>
+            </li>
+            <li> Приём заявок:
+              <span>1 неделя</span>
+            </li>
+          </ul>
+        </div>
+        <div className={`${styles["project-card--recruiting-stage-color"]} ${styles["project-card__footer"]}`}><span>Идёт набор</span></div>
+      </div>
+    </Link>
+  )
+}
---
+++ src/components/projects/project-list/project-list.tsx
@@ -0,0 +1,19 @@
+import styles from "./project-list.module.sass"
+import { ProjectCard } from "../project-card/project-card.tsx";
+
+export const ProjectList = () =>{
+
+  const projectNumber = 0
+
+  return(
+    <div className={styles["project-list"]}>
+      <h2>Проектов найдено: {`${projectNumber}`}</h2>
+      <ul> {/*TODO map*/}
+        <li className={styles["project-list__project"]}><ProjectCard/></li>
+        <li className={styles["project-list__project"]}><ProjectCard/></li>
+        <li className={styles["project-list__project"]}><ProjectCard/></li>
+        <li className={styles["project-list__project"]}><ProjectCard/></li>
+      </ul>
+    </div>
+  )
+}
